0c624acd33aec68af21b69a2d0826045d8218013
Quocanh <phiquocanh717@gmail.com>
2021-07-28 16:03:14 +0700

    đâ

---
+++ src/main/java/com/codegym/service/ICustomerService.java
@@ -0,0 +1,31 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import com.codegym.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class ICustomerService implements IGeneralService<Customer> {
+    @Autowired
+    private ICustomerRepository iCustomerRepository;
+    @Override
+    public Iterable<Customer> findAll() {
+        return iCustomerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return iCustomerRepository.findById(id);
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return iCustomerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+iCustomerRepository.deleteById(id);
+    }
+}
